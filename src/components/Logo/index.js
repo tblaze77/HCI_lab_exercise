a81a79792226a46d417a96fb1f5aba2d915275c8
toni <bzvz@bzvz.com>
2020-11-09 01:51:30 +0100

    adding logo

---
+++ src/components/Logo/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './style.module.css'
+
+const Logo = () => (
+  <section className={styles.logo}>
+    <span className={styles.logoText}>Venice Beach</span>
+    <span className={styles.sub}>CALIFORNIA</span>
+  </section>
+)
+
+export default Logo
