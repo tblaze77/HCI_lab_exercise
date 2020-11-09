e267d408f9f16f20aaacd7bac78427a542f690d8
toni <bzvz@bzvz.com>
2020-11-09 02:08:47 +0100

    refactor tabs using map function

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -2,13 +2,20 @@ import React from 'react'
 
 import styles from './style.module.css'
 
+const navTabs = ["Home", "Accomodation", "Photo gallery", "Contact"]
+
 const NavigationBar = () => (
     <nav className={styles.navigationBar}>
-        <li className={styles.active}>Home</li>
-        <li>Accomodation</li>
-        <li>Photo gallery</li>
-        <li>Contact</li>
-
+       {navTabs.map(tab => {
+           if(tab == "Home")
+           return (
+               <li className={styles.active}>
+                   {tab}
+               </li>
+           )
+           else
+                return <li>{tab}</li>
+       })}
     </nav>
 
 
