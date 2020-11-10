e63475fc5db9ce7b0d7e8ebc2dccc1b76c59b1af
toni <bzvz@bzvz.com>
2020-11-10 03:07:11 +0100

    adding navigationBar to footer

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 import styles from './style.module.css'
 
-const navTabs = ["Home", "Accomodation", "Photo gallery", "Contact"]
+import {navs as navTabs} from "../../constants/const"
 
-const NavigationBar = ({activeTab}) => (
-    <nav className={styles.navigationBar}>
+
+const NavigationBar = ({activeTab, useThisStyle}) => (
+    <nav className={styles[useThisStyle || 'navigationBar']}>
       {navTabs.map(tab => <li className={tab === activeTab ? styles.active : ''}>
         {tab}</li>
       )}
