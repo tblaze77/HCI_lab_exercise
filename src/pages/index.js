6154a12c5315273e885d2c0336e1384511d485bd
toni <bzvz@bzvz.com>
2020-11-10 03:59:11 +0100

    creating page content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import HeaderFooter from "../layouts/HeaderFooter"
 import TitlePage from "../modules/TitlePage"
+import PageContent from "../modules/PageContent"
 
 const IndexPage = () => (
   <HeaderFooter activeTab="Home">
     <TitlePage />
+    <PageContent />
   </HeaderFooter>
 )
 
