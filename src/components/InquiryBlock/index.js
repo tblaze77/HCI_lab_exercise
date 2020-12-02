3cbb8872875b1491a926759c3f55d0b1d2021659
tblaze77 <toni.blazevic77@gmail.com>
2020-12-02 17:05:44 +0100

    page content

--- src/components/InquiryBlock/index.js
+++ src/components/InquiryBlock/index.js
@@ -7,14 +7,14 @@ const InquiryBlock = () =>(
          <div className={styles.dateBoxContainer}>
       <span>Arrival date</span>
       <div className={styles.dateBox}>
-          <img src="https://github.com/n00ne1mportant/PublicFilesRepo/blob/master/appvd-calendar-2.png?raw=true" />
+          <img src="https://github.com/n00ne1mportant/PublicFilesRepo/blob/master/appvd-calendar-2.png?raw=true" alt="slika"/>
           <span>{`${(new Date()).getDate()}.${(new Date()).getMonth() + 1}.${(new Date()).getFullYear()}`}</span>
       </div>
     </div>
     <div className={styles.dateBoxContainer}>
       <span>Departure date</span>
       <div className={styles.dateBox}>
-        <img src="https://github.com/n00ne1mportant/PublicFilesRepo/blob/master/appvd-calendar-2.png?raw=true" />
+        <img src="https://github.com/n00ne1mportant/PublicFilesRepo/blob/master/appvd-calendar-2.png?raw=true" alt="slika" />
         <span>{`${(new Date()).getDate() + 1}.${(new Date()).getMonth() + 1}.${(new Date()).getFullYear()}`}</span>
      </div>
    </div>
