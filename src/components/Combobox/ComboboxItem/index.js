d2fb8c47696b793ae74471f61d3b033d36629594
tblaze77 <toni.blazevic77@gmail.com>
2020-12-03 00:21:26 +0100

    creating Combobox: Internal logic

--- src/components/Combobox/ComboboxItem/index.js
+++ src/components/Combobox/ComboboxItem/index.js
@@ -2,9 +2,10 @@ import React from 'react'
  
 import styles from './style.module.css'
  
-const ComboboxItem = ({language, name, flag}) => (
+const ComboboxItem = ({language, name, flag, setSelectedItem, selected}) => (
     <div key={name}
-    className={styles.comboboxItem}>
+    className={`${styles.comboboxItem} ${selected ? styles.selected : ''}`} 
+    onClick={() => setSelectedItem( {language, name})}>
         <span>{language}</span>
         <img src={flag} alt={name} />
     </div>
--- src/components/Combobox/index.js
+++ src/components/Combobox/index.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
  
 import { languages } from '../../constants/const'
 import ComboboxItem from './ComboboxItem'
 import styles from './style.module.css' 
  
-const Combobox = () =>(
-    <div className={styles.combobox}>
-        {<ul>
-          {languages.map(el => 
-            <ComboboxItem language={el.language}
-              name={el.name} 
-              flag={el.flag} />)}
-        </ul>
-        }
-    </div>
-)
+const Combobox = () =>{
+    const [open, setOpen] = useState(false);
+    const [selectedItem, setSelectedItem] = useState(languages.find(el => el.name=="eng"));
+
+    return (
+        <div className={styles.combobox} onClick={() => setOpen(!open)}>
+             {<ul style={{display: open ? 'flex' : 'none'}}>
+                {languages.map(el => 
+                <ComboboxItem {...el} 
+                setSelectedItem={setSelectedItem}
+                selected={el.name === selectedItem.name}
+                />)}
+            </ul>}
+            {selectedItem.language}
+        </div>
+    )
+    }
+ 
  
 export default Combobox
