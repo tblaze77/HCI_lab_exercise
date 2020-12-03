1c7b2f3dc8ee5a78c083f8b07cd47f7326ad5ecc
tblaze77 <toni.blazevic77@gmail.com>
2020-12-03 01:09:22 +0100

    adding contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,13 @@
 import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
+
+import ContactForm from "../modules/ContactForm";
+import ContactSeparator from "../modules/ContactSeparator";
  
 const ContactPage = () => (
     <HeaderFooterLayout activeTab="Contact">
+        <ContactSeparator/>
+        <ContactForm />
     </HeaderFooterLayout>
 )
  
